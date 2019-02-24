0dd9e488d86b27a68a9fea697aa32e0fea64523e
Chris Morgan <bluemorgan2323@gmail.com>
2019-02-23 23:09:53 -0600

    Fixed submit button feature to reset the input field and the textarea field after button is clicked

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let question = document.getElementById("askQuestion");
-
 function submit() {
     console.log(document.getElementById("contactName").value + " wants to know " + document.getElementById("askQuestion").value);
+    document.getElementById("contactName").value = "";
+    document.getElementById("askQuestion").value = "";
 }
 
 document.getElementById("submitButton").addEventListener("click", submit);
