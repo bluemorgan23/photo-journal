c5f1ee9a32901f77c8c8775775f949ae5b16c237
Chris Morgan <bluemorgan2323@gmail.com>
2019-02-28 09:24:00 -0600

    Declared const for the input fields for the form in photos.html and plugged them into function submit().

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+// Function submit logs the input text and the textarea text values to the console when the submit button is clicked. Then the input fields are both reset.
+
+const contactName = document.querySelector("#contactName");
+const askQuestion = document.querySelector("#askQuestion");
+
 function submit() {
-    console.log(document.getElementById("contactName").value + " wants to know " + document.getElementById("askQuestion").value);
-    document.getElementById("contactName").value = "";
-    document.getElementById("askQuestion").value = "";
+    console.log(contactName.value + " wants to know " + askQuestion.value);
+    // document.getElementById("contactName").value = "";
+    // document.getElementById("askQuestion").value = "";
 }
 
+
 document.getElementById("submitButton").addEventListener("click", submit);
+
+// let imageVAR =  document.querySelector("#img1")
+// imageVAR.addEventListener("click", function() {
+//     let myImage = document.querySelector("#myImg");
+// myImage.setAttribute("src" , `${imageVAR}`)
+// })
